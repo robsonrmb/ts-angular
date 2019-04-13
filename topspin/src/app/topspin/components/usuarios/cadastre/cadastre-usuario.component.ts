c52b6ab68ed0d4e5b85cb36d47bb45b2d3570fd6
robsonrmb <robson.rmb@gmail.com>
2019-04-12 23:13:19 -0300

    Validação assíncrona: teste.

--- topspin/src/app/topspin/components/usuarios/cadastre/cadastre-usuario.component.ts
+++ topspin/src/app/topspin/components/usuarios/cadastre/cadastre-usuario.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControlName, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormControlName, FormControl, FormBuilder, Validators, FormArray, AbstractControl } from '@angular/forms';
 import { Usuario, ChaveValor, Mensagem } from 'src/app/topspin/models';
 import { UsuarioService } from 'src/app/topspin/services';
 import { ESTADOS, NIVEIS, TIPOSCD, SEXOS, MensagemEnum } from 'src/app/topspin/constantes';
+import { Util } from 'src/app/topspin/utils/util';
+import { VerificaEmailService } from 'src/app/topspin/services/util/verifica-email.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cadastre-usuario',
@@ -21,7 +24,8 @@ export class CadastreUsuarioComponent implements OnInit {
   frameworks = ['Angular', 'React', 'Vue', 'Sencha'];
 
   constructor(private formBuilder: FormBuilder,
-              private usuarioService: UsuarioService) { }
+              private usuarioService: UsuarioService,
+              private verificaEmailService: VerificaEmailService) { }
 
   ngOnInit() {
     /*
@@ -41,7 +45,7 @@ export class CadastreUsuarioComponent implements OnInit {
     this.formulario = this.formBuilder.group({
       email: [null, [Validators.required]],
       nome: [null, [Validators.required, Validators.maxLength(40)]],
-      apelido: [null],
+      apelido: [null, [Validators.required]], //validação assincrona: , [this.validarEmail.bind(this)]
       dataNascimento: [null, [Validators.required]],
       estado: [null, [Validators.required]],
       cidade: [null, [Validators.required]],
@@ -50,8 +54,9 @@ export class CadastreUsuarioComponent implements OnInit {
       tipo: [null],
       nivel: [null],
       termos: [null],
-      frameworks: this.buildFrameworks()
-    })
+      frameworks: this.buildFrameworks(),
+      cep: [null, [Validators.required, Util.cepValidator]]
+    }, { validator: Util.equalsToEmail })
 
     this.usuarioService.buscaPorId(this.usuarioService.getUsuario().id)
         .subscribe(
@@ -70,7 +75,8 @@ export class CadastreUsuarioComponent implements OnInit {
 
   buildFrameworks() {
     const values = this.frameworks.map(v => new FormControl(false));
-    return this.formBuilder.array(values);
+    return this.formBuilder.array(values, Util.requiredMinCheckbox(1)); 
+    //this.requiredMinCheckbox(2), neste caso a função estaria aqui neste arquivo/compenent.
     /*
     this.formBuilder.array([
       new FormControl(false),
@@ -79,6 +85,40 @@ export class CadastreUsuarioComponent implements OnInit {
       new FormControl(false)
     ]);
     */
+  }
+
+  requiredMinCheckbox(min = 1) {
+    const validator = (formArray: FormArray) => {
+        /*
+        const values = formArray.controls;
+        let totalChecked = 0;
+        for (let i=0; i<values.length; i++) {
+            if (values[i].value) {
+                totalChecked += 1;
+            }
+        }
+        */
+        const totalChecked = formArray.controls
+            .map(v => v.value)
+            .reduce((total, current) => current ? total + current : total, 0);
+        return totalChecked >= min ? null : { required: true };
+    };
+    return validator;
+  }
+
+  static cepValidator(control: FormControl) {
+    const cep = control.value;
+    if (cep && cep !== '') {
+        const validacep = /^[0-9]{8}$/;
+        return validacep.test(cep) ? null : { cepInvalido : true };
+    }
+  }
+
+  validarEmail(formControl: FormControl) {
+    return this.verificaEmailService.verificarEmail(formControl.value)
+      .pipe(
+        map(emailExiste => emailExiste ? {emailInvalido: true} : null)
+      );
   }
 
   salvar() {
--- topspin/src/app/topspin/utils/util.ts
+++ topspin/src/app/topspin/utils/util.ts
@@ -1,4 +1,6 @@
-class Util {
+import { FormArray, FormControl, AbstractControl } from "@angular/forms";
+
+export class Util {
 
     static converteData_ddMMyyyy_para_yyyyMMdd(data: string): string {
         let dataF = data.substring(6) + '-' +
@@ -14,6 +16,46 @@ class Util {
 					data.substring(0,4);
         console.log(dataF);
         return dataF;
+    }
+
+    static requiredMinCheckbox(min = 1) {
+        const validator = (formArray: FormArray) => {
+            /*
+            const values = formArray.controls;
+            let totalChecked = 0;
+            for (let i=0; i<values.length; i++) {
+                if (values[i].value) {
+                    totalChecked += 1;
+                }
+            }
+            */
+            const totalChecked = formArray.controls
+                .map(v => v.value)
+                .reduce((total, current) => current ? total + current : total, 0);
+            return totalChecked >= min ? null : { required: true };
+        };
+        return validator;
+    }
+
+    static cepValidator(control: FormControl) {
+        const cep = control.value;
+        if (cep && cep !== '') {
+            const validacep = /^[0-9]{8}$/;
+            return validacep.test(cep) ? null : { cepInvalido : true };
+        }
+    }
+
+    static equalsToEmail(group: AbstractControl): {[key: string] : boolean} {
+        const email = group.get('email');
+        const emailConfirmation = group.get('apelido'); //utilizaria o campo de confirmação de email.
+
+        if (!email || !emailConfirmation) {
+            return undefined;
+        }
+        if (email.value !== emailConfirmation.value) {
+            return {emailsNotMatch:true}
+        }
+        return undefined;
     }
 
 }
